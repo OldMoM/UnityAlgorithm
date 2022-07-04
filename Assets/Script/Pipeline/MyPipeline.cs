642665ef66b8a2a7cab9d07d247ee7676665fcaa
OldMoM <1090347251@qq.com>
2022-07-04 23:18:24 +0800

    学习SRP

--- Assets/Script/Pipeline/MyPipeline.cs
+++ Assets/Script/Pipeline/MyPipeline.cs
@@ -5,9 +5,28 @@ using UnityEngine.Rendering;
 
 public class MyPipeline : RenderPipeline
 {
+    private void Buffer(ScriptableRenderContext context, Camera[] cameras)
+    {
+        CommandBuffer buffer = new CommandBuffer
+        {
+            name = "FirstBuffer"
+        };
+        CameraClearFlags clearFlag = cameras[0].clearFlags;
+        buffer.ClearRenderTarget(
+            (clearFlag & CameraClearFlags.Depth) != 0,
+            (clearFlag & CameraClearFlags.Color) != 0,
+            cameras[0].backgroundColor);
+        //buffer.ClearRenderTarget(true, false, Color.clear);
+        context.ExecuteCommandBuffer(buffer);
+        buffer.Release();
+    }
+
     protected override void Render(ScriptableRenderContext context, Camera[] cameras)
     {
-        //base.Render(context, cameras);
+        ScriptableCullingParameters cullingParameters = new ScriptableCullingParameters();
+
+        CullingResults cull = context.Cull(ref cullingParameters);
+
         context.DrawSkybox(cameras[0]);
         context.Submit();
     }
