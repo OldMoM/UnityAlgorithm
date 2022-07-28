a0d02e63e27bb60d5b39a45782a0638fcce9992d
OldMoM <1090347251@qq.com>
2022-07-28 22:53:24 +0800

    树形图

---
+++ Assets/LuaTests/Second.lua
@@ -0,0 +1,11 @@
+function TestSecond_1()
+
+end
+
+function TestSecond_2()
+
+end
+
+function TestSecond_3()
+
+end
---
+++ Assets/Script/LuaTestFramework/TestTreeView.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.IMGUI.Controls;
+using UnityEditor;
+
+public class TestTreeView : TreeView
+{
+	private List<TreeViewItem> m_allItems = new List<TreeViewItem>();
+    private TreeViewItem root = new TreeViewItem() { id = 0, depth = -1, displayName = "Root" };
+
+    static Texture2D[] m_iconTexts =
+    {
+        (Texture2D)EditorGUIUtility.Load("idle.png"),
+        (Texture2D)EditorGUIUtility.Load("pass.png"),
+        (Texture2D)EditorGUIUtility.Load("fail.png"),
+    };
+
+    public TestTreeView(TreeViewState treeViewState)
+		: base(treeViewState)
+	{
+		Reload();
+	}
+
+	protected override TreeViewItem BuildRoot()
+	{
+        SetupParentsAndChildrenFromDepths(root, m_allItems);
+		return root;
+	}
+
+	public void SetDataList()
+    {
+        m_allItems = new List<TreeViewItem>
+            {
+                new TreeViewItem {id = 1, depth = 0, displayName = "Animals",icon = m_iconTexts[0]},
+                new TreeViewItem {id = 2, depth = 1, displayName = "Mammals",icon = m_iconTexts[0]},
+                new TreeViewItem {id = 3, depth = 2, displayName = "Tiger",icon = m_iconTexts[0]},
+                new TreeViewItem {id = 4, depth = 2, displayName = "Elephant",icon = m_iconTexts[0]},
+                new TreeViewItem {id = 5, depth = 2, displayName = "Okapi",icon = m_iconTexts[0]},
+            };
+        this.Reload();
+    }
+}
